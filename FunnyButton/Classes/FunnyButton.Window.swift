aa064bb5ed0add6baf12a430df1e88094ea7a9fb
aa <zhoujianping@52tt.com>
2023-01-18 17:13:20 +0800

    fix little bug

--- FunnyButton/Classes/FunnyButton.Window.swift
+++ FunnyButton/Classes/FunnyButton.Window.swift
@@ -34,7 +34,8 @@ internal extension FunnyButton {
             guard !isHidden, subviews.count > 0 else { return nil }
             for subview in subviews.reversed() where !subview.isHidden && subview.alpha > 0.01 && subview.frame.contains(point) {
                 let subPoint = convert(point, to: subview)
-                return subview.hitTest(subPoint, with: event)
+                guard let rspView = subview.hitTest(subPoint, with: event) else { continue }
+                return rspView
             }
             return nil
         }
